fb67a15f40937e72175f7c2c4d75b3bce470830c
Yacine Soufiane <yacine.soufiane@gmail.com>
2023-10-11 21:17:52 -0400

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -554,8 +554,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>The Pension Penguin</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Introducing Penguin, the chatbot that provides expert answers to all your pension benefits inquiries.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
